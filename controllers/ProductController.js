5f866ee9e1aa80e8acf3fff07b3e08dbfad310f0
Yohanes Harke Wauran <harkewauran23@gmail.com>
2023-05-02 09:25:33 +0800

    first commit

---
+++ controllers/ProductController.js
@@ -0,0 +1,11 @@
+import Product from "../models/ProductModels.js";
+import path from "path";
+import fs from "fs";
+
+export const getProducts = async (req, res) => {};
+
+export const getProductById = async (req, res) => {};
+
+export const createProduct = async (req, res) => {};
+export const deleteProduct = async (req, res) => {};
+export const updateProduct = async (req, res) => {};
---
+++ routes/ProductRoute.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from "../controllers/ProductController.js";
+
+const router = express.Router();
+
+router.get("/products", getProducts);
+router.get("/product/:id", getProductById);
+router.post("/product", createProduct);
+router.patch("/products/:id", updateProduct);
+router.delete("/product/:id", deleteProduct);
+
+export default router;
